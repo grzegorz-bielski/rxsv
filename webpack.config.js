b927a7890c319fd25d26f31e8ec7ac54a5b461f3
Grzegorz Bielski <pesiok@gmail.com>
2019-06-03 11:29:05 +0200

    initial commit

---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+const path = require("path");
+const ForkTsCheckerWebpackPlugin = require("fork-ts-checker-webpack-plugin");
+
+module.exports = {
+  entry: "./src/index.ts",
+  output: {
+    path: path.join(__dirname, "./dist"),
+    libraryTarget: "umd",
+    libraryExport: "default",
+    library: "rxs",
+    filename: "index.js"
+  },
+  plugins: [new ForkTsCheckerWebpackPlugin()],
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        enforce: "pre",
+        loader: "tslint-loader",
+        exclude: /node_modules/
+      },
+      {
+        test: /\.tsx?$/,
+        use: "ts-loader",
+        exclude: /node_modules/
+      }
+    ]
+  },
+  mode: "production",
+  resolve: {
+    extensions: [".ts", ".js"]
+  }
+};
