b3b8f5dd8fd9b7756e523b95c6435836e75f2b49
Grzegorz Bielski <pesiok@gmail.com>
2019-08-23 15:11:47 +0200

    updated build setup

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,7 @@
 module.exports = {
     preset: 'ts-jest',
     testEnvironment: 'node',
+    moduleFileExtensions: ['ts', 'js'],
+    testPathIgnorePatterns: ['/node_modules/', '/dist'],
     collectCoverage: true,
 };
