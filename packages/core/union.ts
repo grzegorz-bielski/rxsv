e9eaad7f0876f9d238c26c773cdf82944366e8a5
Grzegorz Bielski <pesiok@gmail.com>
2019-11-30 18:56:28 +0100

    implemented action union

--- packages/core/union.ts
+++ packages/core/union.ts
@@ -1,40 +1,72 @@
 import { Action, Placeholder } from './types';
+import { createAction } from './createAction';
 
 type ActionType<T> = T extends [infer A, ...unknown[]] ? A : T;
 type Payload<T> = T extends [unknown, infer A, ...unknown[]] ? A : void;
 type CreatorList<T extends unknown[] = Placeholder> = {
     [K in keyof T]: (p: Payload<T[K]>) => { type: ActionType<T[K]>; payload: Payload<T[K]> };
 };
+
 type Union<T extends unknown[]> = T extends [unknown, ...unknown[]] ? CreatorList<T> : never;
 type ExcludeArrayKeys<T> = Exclude<keyof T, keyof unknown[]>;
 type InferedActionTypes<
     T extends { readonly [key: string]: (...args: readonly Placeholder[]) => Placeholder }
 > = ReturnType<T[keyof T]>;
 type InferAction<C extends CreatorList> = InferedActionTypes<Pick<C, ExcludeArrayKeys<C>>>;
+
 type DiscriminateUnion<T, K extends keyof T, V extends T[K]> = T extends Record<K, V> ? T : never;
 type DiscriminateAction<C extends CreatorList, T> = DiscriminateUnion<InferAction<C>, 'type', T>;
+
 type CPayload<C extends CreatorList, T> = DiscriminateAction<C, T>['payload'];
 type CType<C extends CreatorList, T> = DiscriminateAction<C, T>['type'];
+
 type ActionCreator<C extends CreatorList, T> = CPayload<C, T> extends void
     ? () => Action<CType<C, T>>
     : (payload: CPayload<C, T>) => Action<CType<C, T>, CPayload<C, T>>;
-type Creators<C extends CreatorList> = {
-    [T in InferAction<C>['type']]: ActionCreator<C, T>;
-};
-type ActionCreators<C extends unknown[]> = Creators<Union<C & { [n: number]: Placeholder }>>;
+type Creators<C extends CreatorList> = { [T in InferAction<C>['type']]: ActionCreator<C, T> };
+type ActionCreators<C extends unknown[]> = Creators<Union<C>>;
 
 //////
 
-type Foo = ['a', ['b', string], ['cc', { a: number }]];
-type FooCreators = ActionCreators<Foo>;
+export const witness = <T>(): T => void 'non runtime value' as Placeholder;
 
-// declare function union<T>(...types: )
+export const ofType = <T extends string>(type: T) => <P = void>(): [T, P] => [type, witness<P>()];
 
-// function uni
+export function union<T extends Placeholder[]>(
+    ...values: T
+): ActionCreators<{ [P in keyof T]: T[P] }> {
+    return values.reduce(
+        (acc, type) => ({
+            ...acc,
+            [type]: Object.assign(<P>(payload?: P) => createAction(type, payload), { type }),
+        }),
+        {},
+    );
+}
+
+const creators = union(
+    ofType('ADD_TODO')<{ id: string; text: string }>(),
+    ofType('SET_VISIBILITY_FILTER')(),
+    ofType('KEK')(),
+);
+
+// type kek = ReturnType<typeof creators['SET_VISIBILITY_FILTER']>;
+
+creators.SET_VISIBILITY_FILTER();
+// creators.SET_VISIBILITY_FILTER.type
+
+creators.ADD_TODO({ id: '', text: '' });
+// creators.ADD_TODO();
+creators.KEK();
 
 // const Actions = A.union(
-//     ['ADD_TODO', A.payload<{ id: string; text: string }>()],
-//     'SET_VISIBILITY_FILTER',
+// ['ADD_TODO', A.payload<{ id: string; text: string }>()],
+// 'SET_VISIBILITY_FILTER',
+// );
+
+// const Actions = A.union(
+//     A.case('ADD_TODO')<{ id: string; text: string }>(),
+//     A.case('SET_VISIBILITY_FILTER'),
 // );
 
 // type Actions = {
@@ -43,9 +75,9 @@ type FooCreators = ActionCreators<Foo>;
 // }
 
 //   A.reduce(
+//     creators,
 //     {
 //       ADD_TODO: (state, p) =>
 //     },
-//     Actions,
 //     (state, action)
 //   )
